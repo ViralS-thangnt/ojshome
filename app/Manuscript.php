60dc2edcea059dc83f581e508a10cd4b9c9af6a3
thangnt1988 <thangnt@nadia.bz>
2015-03-11 17:56:15 +0700

    commit code

--- app/Manuscript.php
+++ app/Manuscript.php
@@ -33,7 +33,7 @@ class Manuscript extends Model {
 							'send_at'];
 	protected $guarded 	= ['id'];
 
-<<<<<<< HEAD
+
 
 
 	public static function getByStatus($status){
@@ -43,15 +43,6 @@ class Manuscript extends Model {
 
 
 	public static function getInReviewByStatus($status, $user_id){
-		// dd(Manuscript::where('status', '=', $status)
-		// 					->where('manuscripts.author_id', '=', $user_id)
-		// 					->leftJoin('users', 'users.id', '=', 'manuscripts.author_id')
-		// 					->leftJoin('section_manuscripts', function($join){
-		// 						$join->on('users.id', '=', 'section_manuscripts.user_id')
-		// 							->on('manuscripts.id', '=', 'section_manuscripts.manuscript_id');
-		// 					})
-		// 					->select('manuscripts.*', 'users.last_name', 'users.first_name', 'users.middle_name', 'section_manuscripts.*')
-		// 					->get());
 
 		return Manuscript::where('status', '=', $status)
 							->where('manuscripts.author_id', '=', $user_id)
@@ -63,7 +54,8 @@ class Manuscript extends Model {
 							->select('manuscripts.*', 'users.last_name', 'users.first_name', 'users.middle_name', 'section_manuscripts.section_editor_no as round_loop_review')
 							->get();
 
-=======
+	}
+	
 	public function scopeStatus($query, $status, $author_id)
 	{
 		return $query->select('manuscripts.*', 'users.last_name', 'users.first_name', 'users.middle_name')
@@ -71,7 +63,7 @@ class Manuscript extends Model {
 					 ->where('manuscripts.status', '=', $status)
 					 ->where('manuscripts.author_id', '=', $author_id)
 					 ->get();
->>>>>>> 710d2573e7dc789d72157ff4b7138a1309720d13
+
 	}
 
 	public function user(){
