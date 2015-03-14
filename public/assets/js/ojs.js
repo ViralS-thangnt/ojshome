81c2550ead98d1ba270024dae77adc64eeb8302c
Thang Nguyen Toan <toanthang1988@gmail.com>
2015-03-15 04:31:00 +0700

    add count words in create manuscript

--- public/assets/js/ojs.js
+++ public/assets/js/ojs.js
@@ -12,3 +12,22 @@ function countChar(val, max, min, colorok, colorerror, divname) {
 		$('#' + divname).text('Số ký tự có thể nhập thêm: ' + (max - len) + ' ký tự').css({'color' : colorok});
 	}
 }
+
+// Counter words in control
+function countWords(val, max, min, colorok, colorerror, divname){         
+	// s = document.getElementById(val).value;         
+	s = val.value;
+	s = s.replace(/(^\s*)|(\s*$)/gi,"");         
+	s = s.replace(/[ ]{2,}/gi," ");         
+	s = s.replace(/\n /,"\n");         
+	var len = s.split(' ').length; 
+
+	if (len > max) {
+		$('#' + divname).text('Bạn đã nhập quá số từ cho phép. Chỉ được nhập tối đa ' + max + ' từ').css({'color' : colorerror});
+	} else if(len < min) {
+		$('#' + divname).text('Số từ không thể ít hơn ' + min + ' từ').css({'color' : colorerror});
+	} else {
+		$('#' + divname).text('Số từ có thể nhập thêm: ' + (max - len) + ' từ').css({'color' : colorok});
+	}
+
+}
