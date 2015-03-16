6fda199f5bb480e76df57fb97a5e988da5e02e86
truongquan <quandt@NDVs-Mac-mini-2.local>
2015-03-16 14:43:36 +0700

    Map 3 tables to one editor_manuscript

---
+++ database/migrations/2015_03_16_071824_create_editor_manuscript_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEditorManuscriptTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('editor_manuscript', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->tinyInteger('stage');
+			$table->integer('manuscript_id');
+			$table->integer('user_id');
+			$table->integer('loop');
+			$table->text('comments');
+			$table->tinyInteger('decide');
+			$table->integer('editor_suggested_id');
+			$table->string('file');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('editor_manuscript');
+	}
+
+}
