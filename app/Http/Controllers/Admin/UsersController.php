f064cb06df97adb469c8fea02c885e588d08f663
truongquan <quandt@NDVs-Mac-mini-2.local>
2015-03-16 08:24:46 +0700

    update test user controller

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends Controller
     public function __construct(EloquentUserRepository $userRepo)
     {
         Session::put(REQUIRE_PERMISSION, ADMIN);
-        $this->middleware('auth');
+        //$this->middleware('auth');
         $this->userRepo = $userRepo;
     }
 
